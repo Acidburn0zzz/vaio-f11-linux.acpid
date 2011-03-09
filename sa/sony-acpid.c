87346cce8e00feb4f082d3d3da3f39da19c86ec7
VAnhTu1987 <VAnhTu1987@gmail.com>
2011-03-09 23:21:16 +0000

    Use getopt, accept setting als_lux_threshold

--- sa/sony-acpid.c
+++ sa/sony-acpid.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "helpers.h"
 
@@ -9,9 +10,21 @@ static char const*const ACPI_EVENT_FILE = "/proc/acpi/event";
 int main(int argc, char** argv) {
     FILE* const event_fd = open_file(ACPI_EVENT_FILE, "r");
     int bl_ctrl = BC_ACPI;
-    if (argc > 1) {
-        if (!strcmp(argv[1], "nvidia"))
-            bl_ctrl = BC_NVIDIA;
+    int opt = -1;
+
+    while ((opt = getopt(argc, argv, "c:t:")) != -1) {
+        switch (opt) {
+            case 'c':
+                if (!strcmp(optarg, "nvidia"))
+                    bl_ctrl = BC_NVIDIA;
+                break;
+            case 't':
+                write_int_to_file(SONY_ALS_LUX_THRESHOLD, atoi(optarg));
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-c acpi|nvidia] [-t number]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
     }
 
     acpi_event_loop(event_fd, bl_ctrl, &handle_acpi_events);
